20c5340c8113923ec3c7be53d28fecb12e53a2d5
Kaio Antônio <kaioaar@gmail.com>
2022-03-13 19:57:34 -0300

    Projeto Criado

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+import 'package:projeto_alcool_gasolina/Home.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
